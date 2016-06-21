c046f7c87b472efaa1b067b58c9e022d537b89aa
dalong982242260 <zwl19881212>
2016-06-21 14:21:23 +0800

    增加圆形 、三角形、矩形、椭圆形锯齿配置

--- cardvoucher/src/main/java/com/dalong/cardvoucherlayout/CardVoucherLayout.java
+++ cardvoucher/src/main/java/com/dalong/cardvoucherlayout/CardVoucherLayout.java
@@ -5,6 +5,8 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
@@ -21,6 +23,14 @@ public class CardVoucherLayout extends LinearLayout {
 
     public final static int Orientation_ALL=2;// 竖直
 
+    public final static int SHAPE_CIRCLE=0;//圆形
+
+    public final static int SHAPE_RECTANGLE=1;//矩形
+
+    public final static int SHAPE_TRIANGLE=2;//三角
+
+    public final static int SHAPE_ELLIPSE=3;//椭圆
+
     private float mRadius;//半径
 
     private float mSpacing;//间距
@@ -28,6 +38,8 @@ public class CardVoucherLayout extends LinearLayout {
     private int mColor;//颜色
 
     private int mOrientation=Orientation_HORIZONTAL;//方向
+
+    private int mShape=SHAPE_CIRCLE;// 形状
 
     private Paint mPaint;//画笔
 
@@ -56,6 +68,7 @@ public class CardVoucherLayout extends LinearLayout {
         mSpacing=typedArray.getFloat(R.styleable.CardVoucherLayout_CardVoucher_Spacing,8);
         mColor=typedArray.getColor(R.styleable.CardVoucherLayout_CardVoucher_Color, Color.WHITE);
         mOrientation=typedArray.getInt(R.styleable.CardVoucherLayout_CardVoucher_Orientation,Orientation_HORIZONTAL);
+        mShape=typedArray.getInt(R.styleable.CardVoucherLayout_CardVoucher_Shape,SHAPE_CIRCLE);
         typedArray.recycle();
         init();
     }
@@ -136,6 +149,108 @@ public class CardVoucherLayout extends LinearLayout {
      * @param canvas
      */
     private void drawHorizontal(Canvas canvas) {
+        switch (mShape){
+            case SHAPE_CIRCLE:
+                drawHorizontalCircle(canvas);
+                break;
+            case SHAPE_RECTANGLE:
+                drawHorizontalRectangle(canvas);
+                break;
+            case SHAPE_TRIANGLE:
+                drawHorizontalTriangle(canvas);
+                break;
+            case SHAPE_ELLIPSE:
+                drawHorizontalEllipse(canvas);
+                break;
+        }
+
+    }
+
+    /**
+     * 绘制水平的椭圆形
+     * @param canvas
+     */
+    private void drawHorizontalEllipse(Canvas canvas) {
+        for (int i=0;i<mHorizontalNum;i++){
+            float x = mSpacing+mRadius+mResidual_Horizontal_Distance/2+((mSpacing+mRadius*2)*i);
+            // 定义椭圆对象
+            RectF mRectF = new RectF();
+            // 设置椭圆4点
+            mRectF.left =x-mRadius;
+            mRectF.right = x+mRadius;
+            mRectF.top = 0;
+            mRectF.bottom = mRadius;
+            // 绘制上面 椭圆
+            canvas.drawOval(mRectF, mPaint);
+
+            //绘制下面 椭圆 下面和上面的left和right是一样的所以只需要修改top 和bottom就可以了
+            mRectF.top = getHeight()-mRadius;
+            mRectF.bottom = getHeight();
+            canvas.drawOval(mRectF, mPaint);
+        }
+    }
+
+    /**
+     * 绘制水平的三角形
+     * @param canvas
+     */
+    private void drawHorizontalTriangle(Canvas canvas) {
+        for (int i=0;i<mHorizontalNum;i++){
+            float x = mSpacing+mRadius+mResidual_Horizontal_Distance/2+((mSpacing+mRadius*2)*i);
+            // 绘制三角形
+            Path path = new Path();
+            // 绘制上边  三角形的三个点  左面点(x-mRadius,0)   右面点(x+mRadius,0)   下面点(x, mRadius)  然后再回去封闭
+            path.moveTo(x-mRadius,0);
+            path.lineTo(x+mRadius,0);
+            path.lineTo(x, mRadius);
+            path.lineTo(x-mRadius,0);
+            path.close();
+            canvas.drawPath(path,mPaint);
+
+            //绘制下边  绘制上边  三角形的三个点  左面点(x-mRadius,getHeight())   右面点(x+mRadius,getHeight())   上面点(x,getHeight()-mRadius)  然后再回去封闭
+            path.moveTo(x-mRadius,getHeight());
+            path.lineTo(x+mRadius,getHeight());
+            path.lineTo(x,getHeight()-mRadius);
+            path.lineTo(x-mRadius,getHeight());
+            path.close();
+            canvas.drawPath(path,mPaint);
+        }
+    }
+
+    /**
+     * 绘制水平的矩形
+     * @param canvas
+     */
+    private void drawHorizontalRectangle(Canvas canvas) {
+        for (int i=0;i<mHorizontalNum;i++){
+            float x = mSpacing+mRadius+mResidual_Horizontal_Distance/2+((mSpacing+mRadius*2)*i);
+            // 绘制矩形
+            Path path = new Path();
+            // 绘制上边  矩形的4个点  (x-mRadius,0),(x+mRadius,0),(x+mRadius,mRadius),(x-mRadius, mRadius)
+            path.moveTo(x-mRadius,0);
+            path.lineTo(x+mRadius,0);
+            path.lineTo(x+mRadius,mRadius);
+            path.lineTo(x-mRadius, mRadius);
+            path.lineTo(x-mRadius,0);
+            path.close();
+            canvas.drawPath(path,mPaint);
+
+            // 绘制上边  矩形的4个点  (x-mRadius,getHeight()),(x+mRadius,getHeight()),(x+mRadius,getHeight()-mRadius),(x-mRadius,getHeight()-mRadius)
+            path.moveTo(x-mRadius,getHeight());
+            path.lineTo(x+mRadius,getHeight());
+            path.lineTo(x+mRadius,getHeight()-mRadius);
+            path.lineTo(x-mRadius,getHeight()-mRadius);
+            path.lineTo(x-mRadius,getHeight());
+            path.close();
+            canvas.drawPath(path,mPaint);
+        }
+    }
+
+    /**
+     * 绘制水平圆形
+     * @param canvas
+     */
+    private void drawHorizontalCircle(Canvas canvas){
         for (int i=0;i<mHorizontalNum;i++){
             // 水平方向 y值不变 为0和布局的高度2个   x坐标为动态计算  间距+计算好的剩余部分/2+半径+第几个*(间距加上直径)
             float x = mSpacing+mRadius+mResidual_Horizontal_Distance/2+((mSpacing+mRadius*2)*i);
@@ -150,6 +265,107 @@ public class CardVoucherLayout extends LinearLayout {
      * @param canvas
      */
     private void drawVertical(Canvas canvas) {
+        switch (mShape){
+            case SHAPE_CIRCLE:
+                drawVerticalCircle(canvas);
+                break;
+            case SHAPE_RECTANGLE:
+                drawVerticalRectangle(canvas);
+                break;
+            case SHAPE_TRIANGLE:
+                drawVerticalTriangle(canvas);
+                break;
+            case SHAPE_ELLIPSE:
+                drawVerticalEllipse(canvas);
+                break;
+        }
+    }
+    /**
+     * 绘制竖直椭圆形
+     * @param canvas
+     */
+    private void drawVerticalEllipse(Canvas canvas) {
+        for (int i=0;i<mVerticalNum;i++){
+            float y = mSpacing+mRadius+mResidual_Vertical_Distance/2+((mSpacing+mRadius*2)*i);
+            // 定义椭圆对象
+            RectF mRectF = new RectF();
+            // 设置椭圆四个点
+            mRectF.left =0;
+            mRectF.right = mRadius;
+            mRectF.top = y-mRadius;
+            mRectF.bottom = y+mRadius;
+            // 绘制左边 椭圆
+            canvas.drawOval(mRectF, mPaint);
+
+            // 绘制右边  椭圆 右边top和bottom与左面一样不需要修改的
+            mRectF.left = getWidth()-mRadius;
+            mRectF.right = getWidth();
+            canvas.drawOval(mRectF, mPaint);
+        }
+
+    }
+    /**
+     * 绘制竖直三角形
+     * @param canvas
+     */
+    private void drawVerticalTriangle(Canvas canvas) {
+
+        for (int i=0;i<mVerticalNum;i++){
+            float y = mSpacing+mRadius+mResidual_Vertical_Distance/2+((mSpacing+mRadius*2)*i);
+            // 绘制三角形
+            Path path = new Path();
+            // 绘制左边  三角形的3个点  (0,y-mRadius),(0,y+mRadius),(mRadius,y)
+            path.moveTo(0,y-mRadius);
+            path.lineTo(0,y+mRadius);
+            path.lineTo(mRadius,y);
+            path.lineTo(0,y-mRadius);
+            path.close();
+            canvas.drawPath(path,mPaint);
+
+            //绘制右边  三角形的3个点   (getWidth(),y-mRadius),(getWidth(),y+mRadius),(getWidth()-mRadius,y)
+            path.moveTo(getWidth(),y-mRadius);
+            path.lineTo(getWidth(),y+mRadius);
+            path.lineTo(getWidth()-mRadius,y);
+            path.lineTo(getWidth(),y-mRadius);
+            path.close();
+            canvas.drawPath(path,mPaint);
+        }
+    }
+    /**
+     * 绘制竖直矩形
+     * @param canvas
+     */
+    private void drawVerticalRectangle(Canvas canvas) {
+        for (int i=0;i<mVerticalNum;i++){
+            float y = mSpacing+mRadius+mResidual_Vertical_Distance/2+((mSpacing+mRadius*2)*i);
+            // 绘制矩形
+            Path path = new Path();
+            // 绘制左边  矩形的4个点  (0,y-mRadius),(mRadius,y-mRadius),(mRadius,y+mRadius),(0,y-mRadius)
+            path.moveTo(0,y-mRadius);
+            path.lineTo(mRadius,y-mRadius);
+            path.lineTo(mRadius,y+mRadius);
+            path.lineTo(0,y+mRadius);
+            path.lineTo(0,y-mRadius);
+            path.close();
+            canvas.drawPath(path,mPaint);
+
+            // 绘制右边  矩形的4个点  (getWidth(),y-mRadius),(getWidth()-mRadius,y-mRadius),(getWidth()-mRadius,y+mRadius),(getWidth(),y+mRadius)
+            path.moveTo(getWidth(),y-mRadius);
+            path.lineTo(getWidth()-mRadius,y-mRadius);
+            path.lineTo(getWidth()-mRadius,y+mRadius);
+            path.lineTo(getWidth(),y+mRadius);
+            path.lineTo(getWidth(),y-mRadius);
+            path.close();
+            canvas.drawPath(path,mPaint);
+        }
+    }
+
+
+    /**
+     * 绘制竖直圆形
+     * @param canvas
+     */
+    private void drawVerticalCircle(Canvas canvas){
         for (int i=0;i<mVerticalNum;i++){
             // 垂直方向 x值不变 为0和布局的宽度2个   y坐标为动态计算  间距+计算好的剩余部分/2+半径+第几个*(间距加上直径)
             float y = mSpacing+mRadius+mResidual_Vertical_Distance/2+((mSpacing+mRadius*2)*i);
